da437a1d786d8a8e50d1a168bbedac52ebf29bfb
Kuba Krajewski <gim.jakubk@gmail.com>
2022-04-28 22:49:34 +0200

    Correct finetuning and check with london_baseline

--- src/london_baseline.py
+++ src/london_baseline.py
@@ -3,20 +3,13 @@
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
 
-correct = 0
-total = 0
-with open(args.outputs_path, 'w') as fout:
-        predictions = []
-        for line in tqdm(open(args.eval_corpus_path)):
-            x = line.split('\t')[0]
-            x = x + '⁇'
-            x = torch.tensor([pretrain_dataset.stoi[s] for s in x], dtype=torch.long)[None, ...].to(device)
-            pred = utils.sample(gpt_model, x, 32, sample=False)[0]
-            completion = ''.join([pretrain_dataset.itos[int(i)] for i in pred])
-            pred = completion.split('⁇')[1]
-            predictions.append(pred)
-            fout.write(pred + '\n')
-        total, correct = evaluate_places(args.eval_corpus_path, predictions)
-    
-if total > 0:
-        print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+with open('birth_dev.tsv') as fin:
+    lines = [x.strip().split('\t') for x in fin]
+    true_places = [x[1] for x in lines]
+    total = len(true_places)
+    predicted_places = ['London']*total
+    correct = len(list(filter(lambda x: x[0] == x[1],
+      zip(true_places, predicted_places))))
+    total, correct = (float(total),float(correct))
+
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
--- src/run.py
+++ src/run.py
@@ -172,6 +172,7 @@ elif args.function == 'finetune':
 
 
 elif args.function == 'evaluate':
+    print('again here')
     assert args.outputs_path is not None
     assert args.reading_params_path is not None
     assert args.eval_corpus_path is not None
@@ -188,10 +189,12 @@ elif args.function == 'evaluate':
             x = x + '⁇'
             x = torch.tensor([pretrain_dataset.stoi[s] for s in x], dtype=torch.long)[None, ...].to(device)
             pred = utils.sample(gpt_model, x, 32, sample=False)[0]
+            print(pred)
             completion = ''.join([pretrain_dataset.itos[int(i)] for i in pred])
             pred = completion.split('⁇')[1]
             predictions.append(pred)
             fout.write(pred + '\n')
+        print(predictions)
         total, correct = evaluate_places(args.eval_corpus_path, predictions)
     if total > 0:
         print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
